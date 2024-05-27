324b73bdacfd2cf1608065dbd39ed9b560c02605
gatheluck <gatheluck+tech@gmail.com>
2024-05-28 08:37:55 +0900

    feat: update script to generate wordcloud.

--- src/scripts/generate_wordcloud.py
+++ src/scripts/generate_wordcloud.py
@@ -1,5 +1,6 @@
 """Generate word cloud image from paper title and abstract."""
 
+import csv
 import json
 import pathlib
 from typing import Final, Set
@@ -9,17 +10,18 @@ from wordcloud import WordCloud
 from src.utils import Paper
 
 
-def main(
-    paper_json_path: pathlib.Path,
+def generate_wordcloud_from_json(
+    json_path: pathlib.Path,
     image_save_path: pathlib.Path,
     stopwords: Set[str],
     seed: int,
     use_abstract: bool = False,
 ) -> None:
-    """Generate word cloud image from paper title and abstract.
+    """Generate word cloud. All features like Tokenizer or Lemmatizer
+    are comming from WordCloud library.
 
     Args:
-        paper_json_path (pathlib.Path): Path to the JSON file which
+        json_path (pathlib.Path): Path to the JSON file which
             includes paper information.
         image_save_path (pathlib.Path): Path to save the generated word
             cloud image.
@@ -30,7 +32,7 @@ def main(
 
     """
     # Load papers from JSON file.
-    with paper_json_path.open("r") as f:
+    with json_path.open("r") as f:
         papers: list[Paper] = [Paper.model_validate(p) for p in json.load(f)]
 
     # Concat all titles and abstructs.
@@ -49,6 +51,44 @@ def main(
     ).generate(source_text)
     wordcloud.to_file(str(image_save_path))
 
+    print(f"Word cloud image is saved at {image_save_path}.")
+
+
+def generate_wordcloud_from_csv(
+    csv_path: pathlib.Path,
+    image_save_path: pathlib.Path,
+    seed: int,
+) -> None:
+    """Generate word cloud from CSV file.
+
+    Args:
+        csv_path (pathlib.Path): Path to the CSV file which includes
+            text data.
+        image_save_path (pathlib.Path): Path to save the generated word
+            cloud image.
+        seed (int): Random seed for reproducibility.
+
+    """
+    # Load frequency data from CSV file.
+    with csv_path.open("r") as f:
+        reader = csv.reader(f)
+        _ = next(reader)  # Skip header.
+        frequency_dict: dict[str, int] = {row[0]: int(row[1]) for row in reader}
+
+    # Generate word cloud.
+    wordcloud: Final = WordCloud(
+        width=1600,
+        height=800,
+        max_font_size=120,
+        min_font_size=12,
+        background_color="black",
+        random_state=seed,
+    )
+    wordcloud.generate_from_frequencies(frequency_dict)
+    wordcloud.to_file(str(image_save_path))
+
+    print(f"Word cloud image is saved at {image_save_path}.")
+
 
 if __name__ == "__main__":
     import argparse
@@ -59,7 +99,7 @@ if __name__ == "__main__":
         "--input-path",
         "-i",
         type=pathlib.Path,
-        help="An input JSON file path.",
+        help="An input JSON/CSV file path.",
     )
     parser.add_argument(
         "--output-dir",
@@ -88,12 +128,6 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    # Load stopwords.
-    stopwords = set()
-    with args.stopwords_path.open("r") as f:
-        for line in f:
-            stopwords.add(line.strip())
-
     # Check if input file exists.
     if not args.input_path.exists():
         raise FileNotFoundError(f"{args.input_path} does not exist.")
@@ -101,10 +135,27 @@ if __name__ == "__main__":
     # Create output directory if not exists.
     args.output_dir.mkdir(parents=True, exist_ok=True)
 
-    main(
-        args.input_path,
-        args.output_dir / f"{args.input_path.stem}_seed={args.seed}.png",
-        stopwords,
-        args.seed,
-        args.use_abstract,
-    )
+    if args.input_path.suffix == ".csv":
+        generate_wordcloud_from_csv(
+            args.input_path,
+            args.output_dir / f"{args.input_path.stem}_seed={args.seed}.png",
+            args.seed,
+        )
+
+    elif args.input_path.suffix == ".json":
+        # Load stopwords.
+        stopwords = set()
+        with args.stopwords_path.open("r") as f:
+            for line in f:
+                stopwords.add(line.strip())
+
+        generate_wordcloud_from_json(
+            args.input_path,
+            args.output_dir / f"{args.input_path.stem}_seed={args.seed}.png",
+            stopwords,
+            args.seed,
+            args.use_abstract,
+        )
+
+    else:
+        raise ValueError(f"Unsupported file type: {args.input_path.suffix}")
